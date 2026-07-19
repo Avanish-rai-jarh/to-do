ee18148bacb77f86c3f5768ac41834251f51a430
avanishrai,jarh <avanishr2008@gmail.com>
2026-07-19 22:19:48 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     {/* navbar */}
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><i class="fa-solid fa-house"></i></a>
+    <a className="navbar-brand" href="./"><i class="fa-solid fa-house"></i></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
