6d0044bd139679d0f31c5511427ad8d09a321766
avanishrai,jarh <avanishr2008@gmail.com>
2026-07-19 22:59:14 +0530

    resolve

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ function App() {
     {/* navbar */}
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#home"><i class="fa-solid fa-house"></i></a>
+    <a className="navbar-brand" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE"><i class="fa-solid fa-house"></i></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE">Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE">Action</a></li>
+            <li><a className="dropdown-item" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE">Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE">Something else here</a></li>
           </ul>
         </li>
         <li className="nav-item">
