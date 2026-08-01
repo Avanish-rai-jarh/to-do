28e191bb276284beab696d4708cdcc32fca2b080
avanishrai,jarh <avanishr2008@gmail.com>
2026-08-01 21:58:56 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,121 @@
 import './App.css';
+function addchecks(){
+  let task="";
+  let date="";
+  let type="";
+const s = document.querySelector('.con1');
+const over=document.querySelector('.overlay');
+  over.classList.add("overlay1");
+  s.classList.add("style1");
+  s.innerHTML=
+  `<form class="form1">
+    <div class="ip1">
+    <a>What is your task?</a>
+  <textarea type="text" placeholder="Enter task here" class="ip20"/></textarea>
+ </div>
+ <div class="ip1">
+ <a>Pick remainder date and time</a>
+  <input type="datetime-local" placeholder="Date and Time" class="ip21"/>
+ </div>
+ <div class="ip1">
+ <a>Enter task type</a>
+  <input type="dropdown" placeholder="Enter here" class="ip22"/>
+ </div>
+ <div class="buttons">
+ <div class="bt0">
+  <button type="button" class="btn btn-danger">Cancel</button>
+  </div>
+
+ <div class="bt1">
+   <button type="button" class="btn btn-primary">Save Task</button>
+ </div>
+ </div>
+
+ </form>`;
+
+ const cl=document.querySelector('.btn-danger');
+ const sv=document.querySelector('.btn-primary');
+
+ sv.addEventListener("click",()=>{
+  const data=document.querySelector('.todo');
+  task=document.querySelector('.ip20').value;
+  date=document.querySelector('.ip21').value;
+  type=document.querySelector('.ip22').value;
+
+  let datetime=date.split('T');
+  let daywise=datetime[0];
+  let d=new Date(daywise);
+  let day="";
+  let arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  day=arr[d.getDay()]
+
+  if(task!="" && date!="" && type!=""){
+  data.innerHTML+=`<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+  <div>
+  <label class="form-check-label l1" for="checkDefault">
+  ${task}
+  </label>
+
+  <hr>
+
+  <label class="form-check-label l2" for="checkDefault">
+  Day:${day}
+  </label>
+
+  <br>
+
+  <label class="form-check-label l3" for="checkDefault">
+  Date:${datetime[0]}
+  </label>
+
+  <br>
+
+  <label class="form-check-label l4" for="checkDefault">
+  Time:${datetime[1]}
+  </label>
+
+  <div>
+
+  </div>`;
+
+   document.querySelector(".saving").innerHTML="";
+   document.querySelector(".saving").classList.remove("savingStyle");
+
+   document.querySelector('.con1').innerHTML=''; 
+  document.querySelector('.con1').classList.remove('style1');
+  document.querySelector(".overlay").classList.remove("overlay1");
+  }
+
+  if(task=="" || date=="" || type==""){
+    alert("Please fill all the fields");
+  }
+
+  if(data.innerHTML==""){
+    document.querySelector(".saving").classList.add("savingStyle");
+  }
+
+ });
+
+ cl.addEventListener('click', () =>{
+  task=document.querySelector('.ip20').value;
+  date=document.querySelector('.ip21').value;
+  type=document.querySelector('.ip22').value;
+  document.querySelector('.con1').innerHTML=''; 
+  document.querySelector('.con1').classList.remove('style1');
+  document.querySelector(".overlay").classList.remove("overlay1");
+ });
+}
+
 
 function App() {
   return (
     <>
     {/* navbar */}
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE"><i class="fa-solid fa-house"></i></a>
+    <a className="navbar-brand" href="https://www.bing.com/search?q=google&FORM=ANAB01&adppc=EDGEDBB&PC=SCOOBE"><i className="fa-solid fa-house"></i></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -43,8 +152,21 @@ function App() {
 
 {/**add **/}
      <div className="a1">
-      <i class="fa-solid fa-circle-plus"></i>
+      <i className="fa-solid fa-circle-plus" onClick={addchecks}></i>
      </div>
+
+  {/* {collection div} */}
+  <div className="saving savingStyle">
+    <h3 class="default">No tasks added</h3>
+  </div>
+  <div className="todo">
+
+
+  </div>
+
+  {/* {function save form} */} 
+  <div className="con1"></div>
+  <div className="overlay"></div>
 </>
   );
 }
